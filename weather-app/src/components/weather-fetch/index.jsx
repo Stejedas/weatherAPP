27e2be4a9f8831d8371a44681edf48f213c49678
macaeco <macarenafernandezayusob@gmail.com>
2022-03-09 20:43:54 +0100

    he petao a api, no me deja hacer más llamadas

--- weather-app/src/components/weather-fetch/index.jsx
+++ weather-app/src/components/weather-fetch/index.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react"
 
 function WeatherFetch(){
-
-    const [data, updateData]=useState([])
-
-    useEffect(() => {
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=e659f9e2b322c20d032f79bdcd034fa4')
-        .then (j => j.json())
-        .then (data =>updateData(data))
-    }, [])
-    // console.log(data)
-
+    const [weather,updatetWeather] = useState([])
+//     const latFromLocal = localStorage.getItem('lon')
+//    const lonFromLocal = localStorage.getItem('lat') 
+   
+   
+     useEffect(() => {
+       fetch('https://api.openweathermap.org/data/2.5/weather?q=Madrid&appid=e659f9e2b322c20d032f79bdcd034fa4')
+         .then(j => j.json())
+         .then(data => updatetWeather(data))
+     }, [])
+   
     return(
-        <p>{JSON.stringify(data)}</p>
+        <p>{JSON.stringify(weather)}</p>
     )
 }
 export default WeatherFetch;
