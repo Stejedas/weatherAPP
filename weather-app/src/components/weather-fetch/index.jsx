41178c91d3812372e1556551b6a859104e9871af
Sergio Tejeda <stejedas3@gmail.com>
2022-03-11 15:06:26 +0100

    he cambiado los fetch a su sitio, y hemos puesto las card bien ya, y para los 7 dias siguietnes

--- weather-app/src/components/weather-fetch/index.jsx
+++ weather-app/src/components/weather-fetch/index.jsx
@@ -1,6 +1,5 @@
-import { toBePartiallyChecked } from "@testing-library/jest-dom/dist/matchers";
+
 import { useEffect, useContext } from "react"
-import { useBootstrapBreakpoints } from "react-bootstrap/esm/ThemeProvider";
 import { getLatAndLonFromCity, getWeatherByLocation } from "../../api/index.jsx";
 import { weatherContext } from "../context/context.jsx";
 
