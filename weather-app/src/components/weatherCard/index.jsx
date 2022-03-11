5abcf00bbab3ae5bae58192eec3029f2b61baa42
Sergio Tejeda <stejedas3@gmail.com>
2022-03-11 17:20:46 +0100

    Separado el componente de los siguientes 7 dias

--- weather-app/src/components/weatherCard/index.jsx
+++ weather-app/src/components/weatherCard/index.jsx
@@ -10,7 +10,6 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
 import Stack from 'react-bootstrap/Stack'
-import { WiDaySunny, WiDayCloudy, WiCloud, WiCloudy, WiRainMix, WiNightSleet, WiStormShowers, WiSnowflakeCold, WiWindy } from 'react-icons/wi'
 
 import { useEffect, useContext } from "react"
 import { getLatAndLonFromCity, getWeatherByLocation } from "../../api/index.jsx";
@@ -18,6 +17,7 @@ import { weatherContext } from "../context/context.jsx";
 
 
 import './style.css'
+import CardSevenDays from "../CardSevenDays/index.js";
 
 let arraynumber = [1, 2, 3, 4, 5, 6, 7];
 let arrayd = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
@@ -78,49 +78,7 @@ function WeatherCard() {
   }, [lat, lon, unitUse])
 
 console.log(weather)
-  function WriteDay(day){
-    const date = new Date((day) * 1000);
-    const daydate = date.getDay()
-    switch(daydate){
-      case 0: return 'Domingo';
-      break;
-      case 1: return 'Lunes';
-      break;
-      case 2: return 'Martes';
-      break;
-      case 3: return 'Miercoles';
-      break;
-      case 4: return 'Jueves';
-      break;
-      case 5: return 'Viernes';
-      break;
-      case 6: return 'Sábado';
-      break;
-  }
-  }
-
-  function PrintWeather(typeWeahter){
-    switch(typeWeahter){
-      case 'Clear sky': return WiDaySunny();
-      break;
-      case 'Few clouds': return WiDayCloudy();
-      break;
-      case 'Scattered clouds': return WiCloud();
-      break;
-      case 'Broken clouds': return WiCloudy();
-      break;
-      case 'Shower rain': return WiRainMix();
-      break;
-      case 'Rain': return WiNightSleet();
-      break;
-      case 'Thunderstorm': return WiStormShowers();
-      break;
-      case 'Snow': return WiSnowflakeCold();
-      break;
-      case 'Mist': return WiWindy();
-      break; 
-  }}
-
+ 
   return (
     <Container>
       <Row>
@@ -200,28 +158,9 @@ console.log(weather)
           </Stack>
         </Row>
       </Container>
-      <Row>
-        <Col xs={12} > // COMIENZA LAS CARD DIAS POSTERIORES
-          <Stack direction="horizontal" className="d-flex justify-content-between" >
-            { weather.daily?.map((c, i) => {
-              if(i != 0){
-              return (
-                <Card key={c.dt} className='shadowCards'>
-                  
-                    <Card.Title className="title" >
-              {WriteDay(c.dt)}
-                    </Card.Title>
-                    <div className="line"></div>
-                    <Card.Text className="icon">
-                    {PrintWeather(c.weather[0].main)}
-                    </Card.Text>
-                 
-                </Card>
-              );
-            }})}
-          </Stack>
-        </Col>
-      </Row>
+
+    //CARDS 7 DIAS SIGUIENTES //
+    <CardSevenDays infoDays={weather?.daily}></CardSevenDays>
     </Container>
   );
 }
