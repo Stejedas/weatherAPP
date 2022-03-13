c9318dc72d7d1728fd11035d6d1c5c067fc1eb67
macaeco <macarenafernandezayusob@gmail.com>
2022-03-13 16:50:47 +0100

    componente foodCard semiarreglao

--- weather-app/src/api/index.jsx
+++ weather-app/src/api/index.jsx
@@ -6,11 +6,7 @@ import { apikey } from '../api-key/key.js';
 
 export const getLatAndLonFromCity = async (citySearch) => {
     
-
-    let key = apikey;
-
-   
-
+    let key = '4977422d264546168f4144a44e3181c3';
 
     const r = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${key}`)
     const d = await r.json()
@@ -19,14 +15,7 @@ export const getLatAndLonFromCity = async (citySearch) => {
 
 export const getWeatherByLocation = async (lat, lon, unit) => {
 
-
-
-     let key = apikey; 
-
-
-
-
-     
+     let key = '4977422d264546168f4144a44e3181c3'; 
 
     const r = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${unit}&appid=${key}`)
     const d = await r.json()
